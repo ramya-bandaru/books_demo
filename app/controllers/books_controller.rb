4b05e43a8a7735c4645b79c3c04b3ae26f005f53
Ramyasri <ramyasri.a19@gmail.com>
2018-12-13 19:25:35 +0530

    Added update feature

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,23 +5,43 @@ class BooksController < ApplicationController
  end
 
  def new
+   @book = Book.new
+
  end
 
  def create
    @book = Book.new
    @book.update_attributes(name: params[:book][:name] , author: params[:book][:author])
+   if @book.save
+     redirect_to books_path
+   else
+     render :new
+
+
+   end
  end
 
  def update
+   @book = Book.find(params[:id])
+   @book.update_attributes(name: params[:book][:name] , author: params[:book][:author])
+   if @book.save
+     redirect_to books_path
+   else
+     render :edit
+   end
+
  end
 
  def destroy
  end
 
  def edit
+   @book = Book.find(params[:id])
+
  end
 
  def show
+    @book = Book.find(params[:id])
  end
 
 end
